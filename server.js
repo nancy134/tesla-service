6c54f7f12852811d0aa3dc012a5864df415a3549
Nancy Piedra <nancy_piedra@yahoo.com>
2020-01-04 12:10:01 +0000

    Add location api

--- server.js
+++ server.js
@@ -30,14 +30,50 @@ app.get('/vehicles', (req, res) => {
         res.send(err);
     });
 });
+app.get('/vehicle/:id/drive_state', (req, res) => {
+    var token = getToken(req);
+    var driveStatePromise = tesla.drive_state(token,req.params.id);
+    driveStatePromise.then(function(results){
+        res.json(results);
+    }).catch(function(err){
+        res.status(err.statusCode).json(err.error);
+    });
+});
 
 app.get('/vehicle/:id', (req, res) => {
     var token = getToken(req);
+    if (!req.params.id) res.send("id is not defined");
+    else {
     var vehiclePromise = tesla.vehicle(token,req.params.id);
     vehiclePromise.then(function(results){
         res.json(results);
     }).catch(function(err){
-        res.send(err);
+        res.json(err.error);
+    });
+    }
+});
+
+app.get('/vehicle/:id/location', (req, res) => {
+    var token = getToken(req);
+    var driveStatePromise = tesla.drive_state(token,req.params.id);
+    driveStatePromise.then(function(results){
+        var result = {
+            id: req.params.id,
+            latitude: results.response.latitude,
+            longitude: results.response.longitude,
+            speed: results.response.speed
+        };
+
+        res.json(result);
+    }).catch(function(err){
+        var result = {
+            id: req.params.id,
+            latitude: null,
+            longitude: null,
+            speed: null
+        };
+        res.json(result);
     });
 });
+
 app.listen(PORT, HOST);
--- tesla.js
+++ tesla.js
@@ -44,3 +44,25 @@ exports.vehicle = function(access_token, id){
     });
 }
 
+exports.drive_state = function(access_token, id){
+    return new Promise(function(resolve, reject){
+        url = process.env.TESLA_URI + "/api/1/vehicles/" + id +
+            "/data_request/drive_state";
+        var bearerToken = "Bearer "+access_token;
+        var headers = {
+           "Authorization" : bearerToken,
+            "User-Agent" : "NP Software Fleet Management/1.0"
+        };
+        var options = {
+            uri: url,
+            headers: headers,
+            json: true
+        };
+        rp(options).then(function(resp){
+            resolve(resp);
+        })
+        .catch(function(err){
+            reject(err);
+        });
+    });
+}
